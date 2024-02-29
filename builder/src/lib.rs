6ff9ad8e60c5e5765af56ce27a6774b11f9cafc9
H1rono <hronok66@gmail.com>
2024-02-29 16:29:43 +0900

    Answer builder/02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -9,12 +9,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
         vis: _,
         ident,
         generics: _,
-        data: _,
+        data,
     } = parse_macro_input!(input as DeriveInput);
 
+    let builder_ident = format_ident!("{}Builder", ident);
+
+    let Data::Struct(data) = data else {
+        unimplemented!();
+    };
+    let Fields::Named(fields) = data.fields else {
+        unimplemented!();
+    };
+    let builder_fields = fields.named.into_iter().map(|n| {
+        let ident = n.ident.unwrap();
+        let ty = n.ty;
+        quote! {
+            pub #ident: ::std::option::Option<#ty>
+        }
+    });
+
     quote! {
+        #[derive(Default)]
+        struct #builder_ident {
+            #(#builder_fields),*
+        }
+
         impl #ident {
-            pub fn builder() {}
+            pub fn builder() -> #builder_ident {
+                ::std::default::Default::default()
+            }
         }
     }
     .into()
