1d4c83955d3d5a6eba8200c437b6db73ffdb33f5
H1rono <hronok66@gmail.com>
2024-03-04 00:16:55 +0900

    Add `FieldAttribute`

--- builder/src/field.rs
+++ builder/src/field.rs
@@ -1,4 +1,4 @@
-use syn::{Ident, Type};
+use syn::{spanned::Spanned, Attribute, Error, Ident, Type};
 
 #[derive(Clone)]
 pub enum FieldTypeKind {
@@ -15,6 +15,24 @@ pub enum FieldTypeKind {
         angle_close: syn::Token![>],
     },
     Raw(Type),
+}
+
+#[derive(Clone)]
+pub struct FieldAttribute {
+    pub pound_token: syn::Token![#],
+    pub bracket_token: syn::token::Bracket,
+    pub builder: Ident,
+    pub paren_token: syn::token::Paren,
+    pub each_ident: Ident,
+    pub each_eq: syn::Token![=],
+    pub each_val: Ident,
+}
+
+#[derive(Clone)]
+pub struct FieldAttrInner {
+    pub key: Ident,
+    pub eq: syn::Token![=],
+    pub val: syn::LitStr,
 }
 
 #[allow(unused)]
@@ -89,5 +107,56 @@ impl FieldTypeKind {
 
     pub fn is_opt_wrapped(&self) -> bool {
         matches!(self, Self::OptionWrapped { .. })
+    }
+}
+
+#[allow(unused)]
+impl FieldAttribute {
+    pub fn parse_attribute(attribute: Attribute) -> syn::Result<Self> {
+        let span = attribute.meta.span();
+        let err = || Error::new(span, r#"expected `builder(each = "...")`"#);
+        if let syn::AttrStyle::Inner(bang) = &attribute.style {
+            return Err(Error::new(
+                attribute.span(),
+                "`builder` attribute only accepts outer one (the one without `!`)",
+            ));
+        }
+        let syn::Meta::List(meta_list) = attribute.meta else {
+            return Err(err());
+        };
+        let syn::MacroDelimiter::Paren(paren_token) = meta_list.delimiter else {
+            return Err(err());
+        };
+        let builder = meta_list.path.get_ident().cloned().ok_or(err())?;
+        if builder == "builder" {
+            return Err(err());
+        }
+        let FieldAttrInner {
+            key: each_ident,
+            eq: each_eq,
+            val: each_val,
+        } = syn::parse2(meta_list.tokens)?;
+        if each_ident != "each" {
+            return Err(err());
+        }
+        let each_val = syn::parse_str(&each_val.value())?;
+        Ok(Self {
+            pound_token: attribute.pound_token,
+            bracket_token: attribute.bracket_token,
+            builder,
+            paren_token,
+            each_ident,
+            each_eq,
+            each_val,
+        })
+    }
+}
+
+impl syn::parse::Parse for FieldAttrInner {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let key: Ident = input.parse()?;
+        let eq: syn::Token![=] = input.parse()?;
+        let val: syn::LitStr = input.parse()?;
+        Ok(Self { key, eq, val })
     }
 }
