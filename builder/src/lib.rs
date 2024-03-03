2ec74a8e11c5c4e17e73f030d68a47ffa5510712
H1rono <hronok66@gmail.com>
2024-03-03 09:33:04 +0900

    Add module `field`

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,18 +1,13 @@
+mod field;
+
 use std::collections::HashMap;
 
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
 use syn::spanned::Spanned;
-use syn::{
-    parse_macro_input, Data, DeriveInput, Fields, GenericArgument, Ident, PathArguments, Type,
-};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
 
-#[derive(Clone, Copy)]
-enum FieldTypeInfo<'a> {
-    OptionWrapped(&'a Type),
-    VecWrapped(&'a Type),
-    Raw(&'a Type),
-}
+use field::FieldTypeInfo;
 
 struct BuilderAttrArgs {
     each: Ident,
@@ -163,75 +158,6 @@ fn derive_builder(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
         }
     };
     Ok(code)
-}
-
-#[allow(unused)]
-impl<'a> FieldTypeInfo<'a> {
-    pub fn parse(ty: &'a Type) -> Self {
-        macro_rules! filter_try {
-            (let $p:pat = $e:expr) => {
-                let $p = $e else {
-                    return Self::Raw(ty);
-                };
-            };
-            (if $e:expr) => {
-                if $e {
-                    return Self::Raw(ty);
-                }
-            };
-        }
-
-        filter_try!(let Type::Path(p) = ty);
-        filter_try!(if p.qself.is_some() || p.path.leading_colon.is_some());
-        let Some(seg) = p.path.segments.first() else {
-            return Self::Raw(ty);
-        };
-        if seg.ident == "Option" {
-            let PathArguments::AngleBracketed(seg_args) = &seg.arguments else {
-                return Self::Raw(ty);
-            };
-            if seg_args.args.len() != 1 {
-                return Self::Raw(ty);
-            }
-            let Some(GenericArgument::Type(arg_ty)) = seg_args.args.first() else {
-                return Self::Raw(ty);
-            };
-            return Self::OptionWrapped(arg_ty);
-        }
-        if seg.ident == "Vec" {
-            let PathArguments::AngleBracketed(seg_args) = &seg.arguments else {
-                return Self::Raw(ty);
-            };
-            if seg_args.args.len() != 1 {
-                return Self::Raw(ty);
-            }
-            let Some(GenericArgument::Type(arg_ty)) = seg_args.args.first() else {
-                return Self::Raw(ty);
-            };
-            return Self::VecWrapped(arg_ty);
-        }
-        Self::Raw(ty)
-    }
-
-    pub fn as_inner(&'a self) -> &'a Type {
-        match self {
-            Self::OptionWrapped(oty) => oty,
-            Self::VecWrapped(vty) => vty,
-            Self::Raw(rty) => rty,
-        }
-    }
-
-    pub fn is_raw(&self) -> bool {
-        matches!(self, Self::Raw(_))
-    }
-
-    pub fn is_vec_wrapped(&self) -> bool {
-        matches!(self, Self::VecWrapped(_))
-    }
-
-    pub fn is_opt_wrapped(&self) -> bool {
-        matches!(self, Self::OptionWrapped(_))
-    }
 }
 
 impl syn::parse::Parse for BuilderAttrArgs {
